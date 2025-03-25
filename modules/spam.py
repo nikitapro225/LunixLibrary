fb04993ed5c75a7bb47718703a19d4507a00a7ce
nikitapro225 <164543760+nikitapro225@users.noreply.github.com>
2025-03-25 13:09:31 +0300

    Update spam.py

--- modules/spam.py
+++ modules/spam.py
@@ -6,7 +6,11 @@ async def spam(client, message, commandds:dict, config:dict):
     args = message.text.split(" ", 1)
     if len(args) == 3:
         await client.delete_messages(message.chat.id, [message.id])
-        count = int(args[1])
+        try:
+            count = int(args[1])
+        except:
+            await client.edit_message_text(message.chat.id, message.id, f"{config["prefix"]}spam (кол-во:int) (сообщение:str)")
+            return
         reply = message.reply_to_message
 
         if reply:
@@ -14,19 +18,19 @@ async def spam(client, message, commandds:dict, config:dict):
                 for i in range(count):
                     await client.send_photo(message.chat.id, reply.photo.file_id)
                     if (i + 1) % 30 == 0:
-                        sleep(3)
+                        await asyncio.sleep(3)
                 return
             else:
                 for i in range(count):
                     await client.send_message(message.chat.id, reply.text)
                     if (i + 1) % 30 == 0:
-                        sleep(3)
+                        await asyncio.sleep(3)
         else:
             text_to_send = " ".join(args[1:])
             for i in range(count):
                 await client.send_message(message.chat.id, text_to_send)
                 if (i + 1) % 30 == 0:
-                    sleep(3)
+                    await asyncio.sleep(3)
     else:
         await client.edit_message_text(message.chat.id, message.id, f"{config.prefix}spam (кол-во:int) (сообщение:str)")
 
